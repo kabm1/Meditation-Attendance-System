785b2437c00c1ae6d5e0cdee777e0db6c0ba3ff5
aabebe <aabebe@mum.edu>
2019-03-18 11:38:34 -0500

    added annotaions on config classes and a password encoder bean in WebMvcConfig

--- src/main/java/edu/mum/meditaion_attendance/config/SecurityConfiguration.java
+++ src/main/java/edu/mum/meditaion_attendance/config/SecurityConfiguration.java
@@ -1,4 +1,10 @@
 package edu.mum.meditaion_attendance.config;
 
-public class SecurityConfiguration {
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 }
