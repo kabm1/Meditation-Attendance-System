0637f912e6476a1fbfe325a0eb82819d589d476b
mike <mikesol136@gmail.com>
2019-03-18 09:56:53 -0500

    first commit

---
+++ src/main/java/edu/mum/meditaion_attendance/MeditaionAttendanceSytemApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.meditaion_attendance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeditaionAttendanceSytemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeditaionAttendanceSytemApplication.class, args);
+    }
+
+}
