db1dc203e058ae8713a3dc36c659fff860805b0d
Kassahun Assafw <kassamsay@gmail.com>
2019-03-19 00:59:40 -0600

    Course and Location Navigation tab created with thier corresponding view name

--- src/main/java/edu/mum/meditaion_attendance/controller/LocationController.java
+++ src/main/java/edu/mum/meditaion_attendance/controller/LocationController.java
@@ -1,4 +1,15 @@
 package edu.mum.meditaion_attendance.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class LocationController {
+
+    @GetMapping("/location/list")
+    public String getLocation( Model model ){
+          return "location";
+    }
+
 }
