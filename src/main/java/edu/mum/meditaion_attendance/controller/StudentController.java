bafb53c17f48371baedf132cf754f0becf2cee22
aabebe <aabebe@mum.edu>
2019-03-19 10:43:34 -0500

    created student form with validations included

--- src/main/java/edu/mum/meditaion_attendance/controller/StudentController.java
+++ src/main/java/edu/mum/meditaion_attendance/controller/StudentController.java
@@ -5,10 +5,14 @@ import edu.mum.meditaion_attendance.service.StudentService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -31,6 +35,14 @@ public class StudentController {
     @GetMapping("/add")
     public String addStudent(@ModelAttribute("student") Student student,Model model){
         return "StudentForm";
+    }
+    @PostMapping("/add")
+    public String saveStudent(@Valid @ModelAttribute("student") Student student,
+                              BindingResult result, RedirectAttributes redirectAttributes){
+        if(result.hasErrors()){
+            return "StudentForm";
+        }
+        return "redirect:/student/details";
     }
 
 }
